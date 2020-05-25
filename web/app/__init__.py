e1f2356b72af76e786d412a70b2a5b6d22378784
Mun, Younghwan <younghwan.mun@teradata.com>
2020-05-25 19:07:50 -0400

    Move test codes on new added app module.

---
+++ web/app/__init__.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+db = SQLAlchemy()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
+
+
+'''
+import click
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, email):
+        self.email = email
+
+@app.cli.command('create')
+@click.argument('args')
+def create(args): 
+    """Create a database with initial data for 'database' argument."""
+    if (args == 'database'):
+        print("Creating database")
+        db.drop_all()
+        db.create_all()
+        db.session.commit()
+    else:
+        print("Usage: flask [OPTIONS] COMMAND [ARGS]...\n" \
+              "Try 'flask --help' for help.\n\n" \
+              "Error: No such argument '%s'." % args)
+'''    
--- web/wsgi.py
+++ web/wsgi.py
@@ -1,40 +1,5 @@
-import click
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from app import create_app
 
-from config import Config
-
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
-
-    def __init__(self, email):
-        self.email = email
-
-@app.route("/")
-def index():
-    return jsonify(hello="world")
-
-@app.cli.command('create')
-@click.argument('args')
-def create(args): 
-    """Create a database with initial data for 'database' argument."""
-    if (args == 'database'):
-        print("Creating database")
-        db.drop_all()
-        db.create_all()
-        db.session.commit()
-    else:
-        print("Usage: flask [OPTIONS] COMMAND [ARGS]...\n" \
-              "Try 'flask --help' for help.\n\n" \
-              "Error: No such argument '%s'." % args)
-
+app = create_app()
 if __name__ == "__main__":
     app.run()
