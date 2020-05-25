af25501b928962a3a6ea0128a57bb42fc5fa901a
Mun, Younghwan <younghwan.mun@teradata.com>
2020-05-25 00:04:45 -0400

    Add flask-sqlalchemy

--- doc/frequent-usage-docker-cmd.md
+++ doc/frequent-usage-docker-cmd.md
@@ -18,6 +18,7 @@ $ docker volume create --name=postgres_volume
 ```
 $ docker rm -f                      # Remove one or more containers
 $ docker rmi                        # Remove one or more images
+$ docker volume rm                  # Remove one or more volumes
 $ docker system prune -a            # Remove unused data
 $ docker volume prune               # Remove all unused local volumes
 $ docker network prune              # Remove all unused networks
@@ -27,8 +28,15 @@ $ docker network prune              # Remove all unused networks
 ```
 $ docker exec -it                   # Run a command in a running container
 $ docker exec -it web /bin/bash
+$ docker exec -it postgres psql --username='postgres' --dbname='web'
+```
+
+### Run 
+```
+$ docker run                        # Run a command in a new container
 $ docker run -it --rm web:custom /bin/bash
 $ docker run --name web -p 5000:5000 -d --rm web:custom
+$ docker run --name postgres -p 54320:5432 --env-file ./.env.db -d --rm postgres:13-alpine
 ```
 
 ### Build
--- web/wsgi.py
+++ web/wsgi.py
@@ -1,7 +1,22 @@
 import click
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, email):
+        self.email = email
 
 @app.route("/")
 def index():
@@ -10,9 +25,12 @@ def index():
 @app.cli.command('create')
 @click.argument('args')
 def create(args): 
-    """Create an initial database for 'db' argument."""
-    if (arg == 'db'):
-        print("creating db")
+    """Create a database with initial data for 'database' argument."""
+    if (args == 'database'):
+        print("Creating database")
+        db.drop_all()
+        db.create_all()
+        db.session.commit()
     else:
         print("Usage: flask [OPTIONS] COMMAND [ARGS]...\n" \
               "Try 'flask --help' for help.\n\n" \
