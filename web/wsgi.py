ce7560e6070a9b26355fade753660a8ef71fd50b
Mun, Younghwan <younghwan.mun@teradata.com>
2020-05-24 20:15:23 -0400

    Add 'create db' command

--- web/wsgi.py
+++ web/wsgi.py
@@ -1,3 +1,4 @@
+import click
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -5,6 +6,17 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return jsonify(hello="world")
+
+@app.cli.command('create')
+@click.argument('args')
+def create(args): 
+    """Create an initial database for 'db' argument."""
+    if (arg == 'db'):
+        print("creating db")
+    else:
+        print("Usage: flask [OPTIONS] COMMAND [ARGS]...\n" \
+              "Try 'flask --help' for help.\n\n" \
+              "Error: No such argument '%s'." % args)
 
 if __name__ == "__main__":
     app.run()
