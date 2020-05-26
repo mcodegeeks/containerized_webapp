bb7b1380db278562458d018f96deec52dc02e656
Mun, Younghwan <younghwan.mun@teradata.com>
2020-05-26 16:37:48 -0400

    Add flask shell context

--- web/wsgi.py
+++ web/wsgi.py
@@ -1,5 +1,12 @@
-from app import create_app
+from app import create_app, db
+from app.auth.models import User
 
 app = create_app()
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
+    # u = User(email='susan@example.com', username='susan')
+
 if __name__ == "__main__":
     app.run()
