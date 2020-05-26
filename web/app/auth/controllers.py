4d5b9b78bbd6827362ff53a30b3568282c314bbd
Mun, Younghwan <younghwan.mun@teradata.com>
2020-05-26 15:35:23 -0400

    Add authentication forms.

--- web/app/auth/controllers.py
+++ web/app/auth/controllers.py
@@ -1,11 +1,40 @@
 from flask import render_template, redirect, url_for, flash
 from app.auth import bp
-from app.auth.views import RegisterForm
+from app.auth.views import LoginForm, RegisterForm, ForgotPasswordForm, ResetPasswordForm
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('main.index'))
+    return render_template('login.html', title='Sign in', form=form)
+
 
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('main.index'))
-    return render_template('register.html', title='Register', form=form)
+        flash('You have been registered successfully. Try signing in with it here.')
+        return redirect(url_for('auth.login'))
+    return render_template('register.html', title='Sign up', form=form)
+
+
+@bp.route('/forgotpassword', methods=['GET', 'POST'])
+def forgotPassword():
+    form = ForgotPasswordForm()
+    if form.validate_on_submit():
+        return redirect(url_for('auth.resetPassword'))
+    '''
+    There was a problem
+    We're sorry. We weren't able to identify you given the information provided.
+    '''
+    return render_template('forgot_password.html', title='Forgot password', form=form)
+
+
+@bp.route('/resetpassword', methods=['GET', 'POST'])
+def resetPassword():
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        flash('Your password has been changed successfully. Try signing in with it here.')
+        return redirect(url_for('auth.login'))
+    return render_template('reset_password.html', title='Reset password', form=form)
--- web/app/auth/views.py
+++ web/app/auth/views.py
@@ -2,9 +2,26 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
+class LoginForm(FlaskForm):
+    email = StringField('E-mail addres', validators=[DataRequired(), Email()])
+    submit = SubmitField('Continue')
+
 class RegisterForm(FlaskForm):
     username = StringField('Your name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('E-mail address', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Create your account')
+
+    '''
+    Passwords must consist of at least 6 characters.
+    '''
+
+class ForgotPasswordForm(FlaskForm):
+    email = StringField('E-mail address', validators=[DataRequired(), Email()])
+    submit = SubmitField('Continue')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('New password', validators=[DataRequired()])
+    password2 = PasswordField('Password again', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Save changes and sign in')
